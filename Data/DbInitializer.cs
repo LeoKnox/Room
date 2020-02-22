b59ca7c28ec60b9c3a38c19b5ca016da0a3bb829
LeoKnox <LeoKnox@users.noreply.github.com>
2020-02-21 23:11:06 -0700

    Add files via upload

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MvcRoom.Data;
+using MvcRoom.Models;
+
+namespace MvcRoom.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(DungeonContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Room.Any())
+            {
+                return;
+            }
+
+            var rooms = new Room[]
+            {
+                new Room{Name="Entry",Style="Wood",Length=5,Width=5},
+                new Room{Name="Hall",Style="Stone",Length=6,Width=4 }
+            };
+            foreach (Room r in rooms)
+            {
+                context.Room.Add(r);
+            }
+            context.SaveChanges();
+
+            var doors = new Door[]
+            {
+                new Door{DoorID=1,Design="Iron",Location=3},
+                new Door{DoorID=4,Design="Secret",Location=4}
+            };
+            foreach (Door d in doors)
+            {
+                context.Door.Add(d);
+            }
+            context.SaveChanges();
+
+            var features = new Feature[]
+            {
+                new Feature{RoomID=1,DoorID=1,Inside=Inside.Item},
+                new Feature{RoomID=4, DoorID=2,Inside=Inside.Monster}
+            };
+            foreach (Feature f in features)
+            {
+                context.Features.Add(f);
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- Data/DungeonContext.cs
+++ Data/DungeonContext.cs
@@ -17,5 +17,12 @@ namespace MvcRoom.Data
         public DbSet<Room> Room { get; set; }
         public DbSet<Feature> Features { get; set; }
         public DbSet<Door> Door { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Door>().ToTable("Course");
+            modelBuilder.Entity<Feature>().ToTable("Feature");
+            modelBuilder.Entity<Room>().ToTable("Table");
+        }
     }
 }
